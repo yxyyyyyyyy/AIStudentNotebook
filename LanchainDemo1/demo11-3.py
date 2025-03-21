a5a8e0698c362a1f20e5bf46be4f13825bcbae35
yxyyyyyyyy <3436771164@qq.com>
2025-03-21 15:08:10 +0800

    更改文件结构

---
+++ LanchainDemo1/demo11-3.py
@@ -0,0 +1,45 @@
+import os
+
+from langchain.chains.combine_documents.map_reduce import MapReduceDocumentsChain
+from langchain.chains.combine_documents.reduce import ReduceDocumentsChain
+from langchain.chains.combine_documents.stuff import StuffDocumentsChain
+from langchain.chains.llm import LLMChain
+from langchain.chains.summarize import load_summarize_chain
+from langchain_community.document_loaders import WebBaseLoader
+from langchain_core.prompts import PromptTemplate
+from langchain_core.runnables import RunnablePassthrough
+from langchain_openai import ChatOpenAI
+from langchain_text_splitters import CharacterTextSplitter
+
+# 设置代理（如果需要）
+os.environ["HTTPS_PROXY"] = "http://127.0.0.1:7897"
+os.environ["HTTP_PROXY"] = "http://127.0.0.1:7897"
+os.environ["LANGCHAIN_PROJECT"] = "LangchainDemo3"
+os.environ["TAVILY_API_KEY"] = "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"
+
+
+# 创建模型
+model = ChatOpenAI(
+    openai_api_key="sk-84f2472c5c8042e0a23927b4176bdfe9",
+    base_url="https://api.deepseek.com/v1",
+    model_name="deepseek-chat"
+)
+
+# 加载我们的文档。我们将使用 WebBaseLoader 来加载博客文章：
+loader = WebBaseLoader('https://lilianweng.github.io/posts/2023-06-23-agent/')
+docs = loader.load()  # 得到整篇文章
+
+
+# 第三种： Refine
+'''
+Refine: RefineDocumentsChain 类似于map-reduce：
+文档链通过循环遍历输入文档并逐步更新其答案来构建响应。对于每个文档，它将当前文档和最新的中间答案传递给LLM链，以获得新的答案。
+'''
+text_splitter = CharacterTextSplitter.from_tiktoken_encoder(chunk_size=1000, chunk_overlap=0)
+split_docs = text_splitter.split_documents(docs)
+# 指定chain_type为： refine
+chain = load_summarize_chain(model, chain_type='refine')
+
+result = chain.invoke(split_docs)
+print(result['output_text'])
+
